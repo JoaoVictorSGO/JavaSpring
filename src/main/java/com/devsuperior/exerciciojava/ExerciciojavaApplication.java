abe5aeabfa9f76dca09ba84530de7f6d9d210c4c
Joao <jvictorsgon@gmail.com>
2025-01-25 11:04:45 -0300

    Primeiro commit

---
+++ src/main/java/com/devsuperior/exerciciojava/ExerciciojavaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exerciciojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciojavaApplication.class, args);
+	}
+
+}
